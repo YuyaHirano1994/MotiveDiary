44c4e13d6f171aa163f0e3ba54015f9ee114e9ff
Yuya Hirano <v322sk@gmail.com>
2023-05-12 19:04:53 -0700

    refa

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { RecoilRoot } from "recoil";
+import registerServiceWorker from "./registerServiceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -9,3 +10,5 @@ root.render(
     <App />
   </RecoilRoot>
 );
+
+registerServiceWorker();
